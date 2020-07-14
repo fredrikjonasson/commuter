5b59a124f10ed983e45ac8d34aea3c78359921f3
Fredrik Jonasson <fredrik.jonasson@outlook.com>
2020-07-14 21:10:12 +0200

    Some restructuring

--- uppsalastockholm/controller.php
+++ uppsalastockholm/controller.php
@@ -1,42 +1,45 @@
 <?php
 require_once 'apirequest.php';
 //require_once('databasehandler.php');
-require_once 'timetreshold.php';
+require_once '../timetreshold.php';
 
-class Controller{
+class Controller
+{
 	private $DatabaseHandler;
 	private $Timetreshold;
 
-	function __construct(){
+	function __construct()
+	{
 		$this->SetupAndPopulateDatabaseHandler();
 		$this->Timetreshold = new Timetreshold();
 		$this->GetRequest();
 	}
 
-	function SetupAndPopulateDatabaseHandler(){
+	function SetupAndPopulateDatabaseHandler()
+	{
 		$this->DatabaseHandler = new DatabaseHandler();
 		$this->DatabaseHandler->FetchDeparturesAndTime();
 		$this->DatabaseHandler->CloseConnection();
 	}
 
-	function GetRequest(){
+	function GetRequest()
+	{
 		$TimeOfLastApiCall = $this->DatabaseHandler->ReturnLastCallTime();
 		if ($this->Timetreshold->IsTresholdReached($TimeOfLastApiCall)) {
-			echo($this->CallApi());
+			echo ($this->CallApi());
 		} else {
-			echo($this->DatabaseHandler->ReturnLastApiAnswer());
+			echo ($this->DatabaseHandler->ReturnLastApiAnswer());
 		}
 	}
 
-	function CallApi(){
+	function CallApi()
+	{
 		$apiCall = new ApiRequest();
 		$departures = $apiCall->AskApi();
-		return($departures);
+		return ($departures);
 	}
-
 }
 //$testar = time(); 1594435500 45 1594479190 NU 43690
 
 $hej = new Controller();
 //var_dump((int)date('i', 43690));
-?>
